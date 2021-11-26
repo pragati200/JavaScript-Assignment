53854d9f8ecf9a30ed1f4750f4f9357481e9a88e
pragati200 <90255949+pragati200@users.noreply.github.com>
2021-11-26 23:38:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var person1 = {
+    FirstName: "Pragati",
+    LastName: "Priya",
+    OfficeAdress: "Patna , Bihar 800025",
+  };
+  
+  var person2 = {
+    FirstName: "Ram",
+    LastName: "Kumar",
+    OfficeAdress: "Kolkata, West Bengal 789654",
+  };
+  
+  var person3 = {
+    FirstName: "Navin",
+    LastName: "Kumar",
+    OfficeAdress: "Pune, Maharashtra 123456",
+  };
+  
+  console.log(person1);
+  console.log(person2);
+  console.log(person3);
